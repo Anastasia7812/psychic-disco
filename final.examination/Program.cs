3d3e9d1ef8e6a710498eb50d539b27d011542682
Ya <gileva.anastasia@bk.ru>
2023-11-26 13:51:58 +0500

    задача решена

--- final.examination/Program.cs
+++ final.examination/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача: Написать программу, 
+// 1. которая из имеющегося массива строк
+// 2. формирует новый массив из строк, (Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.)
+// 3. длина которых меньше, либо равна 3 символам.
+
+
+//Примеры:
+//[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+//[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+//[“Russia”, “Denmark”, “Kazan”] → []
+
+string[] array1 = new string[5] {"dfjkyfb", "gi", "hello", "123", "rjo"};
+
+string[] array2 = new string[array1.Length];
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+
+
+{
+    int count = 0;
+
+    for (int i = 0; i < array1.Length; i++)
+
+
+    {
+    if(array1[i].Length <= 3)
+
+
+        {
+        array2[count] = array1[i];
+
+        count++;
+        }
+
+
+    }
+}
+
+
+void PrintArray(string[] array)
+
+
+{
+    for (int i = 0; i < array.Length; i++)
+
+
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+
+    Console.WriteLine();
+
+}
+
+
+SecondArrayWithIF(array1, array2);
+
+PrintArray(array2);
